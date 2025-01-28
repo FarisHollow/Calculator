ad9c1edd08573a446bbf28e374cebcca66cd9c48
MinuarTanzir <minuartanzir@gmail.com>
2025-01-28 17:36:16 +0600

    updated code

--- app/core/operations.py
+++ app/core/operations.py
@@ -6,4 +6,11 @@ def sub(a, b):
     """Return the sub of two numbers."""
     return a - b
 
+def mul(a, b):
+    """Return the mul of two numbers."""
+    return a * b
+
+def div(a, b):
+    """Return the div of two numbers."""
+    return a / b
 
